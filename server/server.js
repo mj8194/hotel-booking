a1d8f336a5c88de97dac55f150fc4e417173b724
Subhiksha Jain <mj8194328@gmail.com>
2026-01-23 21:58:23 +0530

    Enhance CORS configuration with methods and headers

--- server/server.js
+++ server/server.js
@@ -35,6 +35,8 @@ app.use(cors({
     }
   },
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 // ---------------------- Webhooks ----------------------
