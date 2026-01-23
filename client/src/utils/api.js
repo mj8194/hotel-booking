05ca1f8e4a0dc9c1bd8305aa02342129e4d862bd
Subhiksha Jain <mj8194328@gmail.com>
2026-01-23 21:12:28 +0530

    fix api base url for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: baseURL
 });
+
 export default api;
